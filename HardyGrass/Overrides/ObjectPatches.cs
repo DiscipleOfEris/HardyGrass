0c3704258a63bb98bfa6caab195bb6b3dc63148b
DiscipleOfEris <discipleoferis1@gmail.com>
2021-05-21 09:35:26 -0700

    v1.0.1
    Initial release

---
+++ HardyGrass/Overrides/ObjectPatches.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.TerrainFeatures;
+using Harmony;
+
+namespace HardyGrass
+{
+    [HarmonyPatch(typeof(StardewValley.Object))]
+    [HarmonyPatch("placementAction", new Type[] { typeof(GameLocation), typeof(int), typeof(int), typeof(Farmer) })]
+    public class Object_placementAction_Patch
+    {
+        public static bool Prefix(StardewValley.Object __instance, ref bool __result, GameLocation location, int x, int y, Farmer who)
+        {
+            if (__instance.bigCraftable || __instance is StardewValley.Objects.Furniture || (__instance.ParentSheetIndex != ModEntry.GrassStarterObjectId && __instance.ParentSheetIndex != ModEntry.QuickGrassStarterObjectId))
+            {
+                return true;
+            }
+
+            Vector2 placementTile = new Vector2(x / 64, y / 64);
+
+            if (location.objects.ContainsKey(placementTile) || location.terrainFeatures.ContainsKey(placementTile))
+            {
+                __result = false;
+                return false;
+            }
+
+            Grass grass = new Grass(1, ModEntry.config.shortGrassStarters ? 0 : 4);
+            if (__instance.ParentSheetIndex == ModEntry.QuickGrassStarterObjectId)
+            {
+                grass.modData.Add(ModEntry.IsQuickModDataKey, ModEntry.IsQuickModDataValue);
+            }
+            location.terrainFeatures.Add(placementTile, grass);
+            location.playSound("dirtyHit");
+
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(StardewValley.Object))]
+    [HarmonyPatch("isPlaceable", new Type[] { })]
+    public class Object_isPlaceable_Patch
+    {
+        public static void Postfix(StardewValley.Object __instance, ref bool __result)
+        {
+            if (!__instance.bigCraftable && __instance.ParentSheetIndex == ModEntry.QuickGrassStarterObjectId)
+            {
+                __result = true;
+                return;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(StardewValley.Object))]
+    [HarmonyPatch("isPassable", new Type[] { })]
+    public class Object_isPassable_Patch
+    {
+        public static void Postfix(StardewValley.Object __instance, ref bool __result)
+        {
+            if (!__instance.bigCraftable && __instance.ParentSheetIndex == ModEntry.QuickGrassStarterObjectId)
+            {
+                __result = true;
+                return;
+            }
+        }
+    }
+}
